d03dccbf260f07c7c838c3849559ac41f3f569d1
weronikakolodziej <77166771+weronikakolodziej@users.noreply.github.com>
2022-07-09 23:46:36 +0200

    Update routes.py

--- Documents/rozne/kodowanie/flask_app/mongoapp/main/routes.py
+++ Documents/rozne/kodowanie/flask_app/mongoapp/main/routes.py
@@ -3,12 +3,11 @@ from re import I
 from flask import Blueprint, render_template, redirect, url_for, request
 from mongoapp.extensions import mongo
 
-#from mongoapp.aggregations import users, requests, systems
+
 
 main = Blueprint('main', __name__)
 
 
 @main.route('/')
 def index():
-   # return render_template('index.html', results_users=users, results_requests=requests, results_systems=systems)
     return render_template('index.html', results_users='users', results_requests='requests', results_systems='systems')
